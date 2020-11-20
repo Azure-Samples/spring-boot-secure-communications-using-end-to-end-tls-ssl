d03ee87ffec7f90d8791878a21c81e8c432dca10
Jeff <1830237+domainname@users.noreply.github.com>
2020-11-21 00:58:03 +0800

    Use discovery functionality of Eureka, hence remove env vars GREETING_SERVICE & GREETING_EXTERNAL_SERVICE (#9)
    * Hard code gateway routing rule and enable SSL in gateway.
    Move RestTemplate out of GreetingController class.
    
    * Remove hard-coded gateway routing rule. Update guide in README.

--- greeting-external-service/src/main/java/com/secure/greeting/GreetingApplication.java
+++ greeting-external-service/src/main/java/com/secure/greeting/GreetingApplication.java
@@ -6,9 +6,21 @@
 
 package com.secure.greeting;
 
+import java.security.KeyStore;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,6 +28,28 @@ public class GreetingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() throws Exception {
+		KeyStore ks = KeyStore.getInstance("AzureKeyVault");
+		SSLContext sslContext = SSLContexts.custom()
+			.loadTrustMaterial(ks, new TrustSelfSignedStrategy())
+			.build();
+
+		HostnameVerifier allowAll = (String hostName, SSLSession session) -> true;
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, allowAll);
+
+		CloseableHttpClient httpClient = HttpClients.custom()
+			.setSSLSocketFactory(csf)
+			.build();
+
+		HttpComponentsClientHttpRequestFactory requestFactory
+			= new HttpComponentsClientHttpRequestFactory();
+
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		return restTemplate;
 	}
 
 }
